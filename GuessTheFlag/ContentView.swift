c706010f26d07d494b8ec3ce0f47c344d4348d8b
SantiGO055 <santigonzalez05@gmail.com>
2023-12-17 17:00:04 -0300

    .

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            /*
+             LinearGradient(stops: [
+                 .init(color: .white, location: 0.45),
+                 .init(color: .black, location: 0.55),
+             ], startPoint: .top, endPoint: .bottom)
+             
+             LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+             
+             RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+             
+            AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)*/
+            /*Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+             
+             Text("Your content")
+                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+                 .foregroundStyle(.white)
+                 .background(.red.gradient)*/
+            Button("Delete selection") {
+                print("Now deleting…")
+            }
         }
-        .padding()
+        .ignoresSafeArea()
+        
     }
 }
 
