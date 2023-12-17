f3407c89b7213bf9731d633399f530025e7b26c5
SantiGO055 <santigonzalez05@gmail.com>
2023-12-17 17:07:36 -0300

    .

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,7 +11,44 @@ struct ContentView: View {
     var body: some View {
         Button("Delete selection", action: executeDelete)
         
-        
+        Button("Delete selection", role: .destructive, action: executeDelete)
+        VStack {
+            Button("Button 1") { }
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) { }
+                .buttonStyle(.bordered)
+            Button("Button 3") { }
+                .buttonStyle(.borderedProminent)
+            Button("Button 4", role: .destructive) { }
+                .buttonStyle(.borderedProminent)
+            Button("Button 3") { }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button {
+                print("Button was tapped")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Image(systemName: "pencil")
+            }
+            Button("Edit", systemImage: "pencil") {
+                print("Edit button was tapped")
+            }
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+        }
     }
     func executeDelete(){
         print("Now deleting…")
