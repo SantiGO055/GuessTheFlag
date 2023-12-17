9195b3d55ae8a54747fc2ad31622d9975f60fbbb
SantiGO055 <santigonzalez05@gmail.com>
2023-12-17 17:25:00 -0300

    .

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,47 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
     var body: some View {
-        Button("Delete selection", action: executeDelete)
-        
-        Button("Delete selection", role: .destructive, action: executeDelete)
-        VStack {
-            Button("Button 1") { }
-                .buttonStyle(.bordered)
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-            Button {
-                print("Button was tapped")
-            } label: {
-                Text("Tap me!")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.red)
-            }
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Image(systemName: "pencil")
-            }
-            Button("Edit", systemImage: "pencil") {
-                print("Edit button was tapped")
-            }
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.red)
-            }
-        }
+        Button("Show Alert") {
+                    showingAlert = true
+                }
+                .alert("Important message", isPresented: $showingAlert) {
+                    
+                    Button("OK", role: .cancel) { }
+                }message: {
+                    Text("Please read this.")
+                }
     }
     func executeDelete(){
         print("Now deleting…")
